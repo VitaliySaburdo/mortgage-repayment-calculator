ce154f560d8fb4b370db56c2b1575f79b32fc97f
VitaliySaburdo <vsaburdo@gmail.com>
2024-07-12 20:03:38 +0300

    input validation

--- main.js
+++ main.js
@@ -1,38 +1,27 @@
-
 const form = document.querySelector('form');
 const mortgageAmount = document.getElementById('amount');
 const mortgageTerm = document.getElementById('term');
 const interestRate = document.getElementById('rate');
-const mortgageType = document.querySelector('input[name="mortgage-type"]:checked');
+const mortgageType = document.querySelector('input[name="mortgage-type"]');
 const calculateBtn = document.getElementById('btn');
 const errorMessage = document.querySelector('.error_message');
 
+form.addEventListener('submit', handleSubmit);
 
-form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const amount = parseFloat(form.elements.amount.value);
+  const term = parseInt(form.elements.term.value);
+  const rate = parseFloat(form.elements.rate.value);
 
-function handleSubmit (event) {
-    event.preventDefault();
-    const amount = parseFloat(form.elements.amount.value);
-    const term = parseInt(form.elements.term.value);
-    const rate = parseFloat(form.elements.rate.value);
+  if (!amount || !term || !rate || !mortgageType.checked) {
+    console.log('error');
+    return;
+  }
 
-    const monthlyRate = rate / 100 / 12;
-    const monthlyPayment = (amount * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -term));
-    const totalPayment = monthlyPayment * term;
+  console.log(amount);
 
-
-    console.log(monthlyPayment.toFixed(2));
-    
-    console.log(totalPayment.toFixed(2));
-    
+  const monthlyRate = rate / 100 / 12;
+  const monthlyPayment = (amount * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -term));
+  const totalPayment = monthlyPayment * term;
 }
-
-
-mortgageAmount.addEventListener('input', function(e) {
-    const pattern = new RegExp('^\\d+([.,]\\d{1,2})?$');
-    if (!pattern.test(e.target.value)) {
-        errorMessage.style.visibility = 'visible';
-    } else {
-        errorMessage.style.visibility = 'hidden';
-    }
-});
