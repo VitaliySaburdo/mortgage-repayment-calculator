ec82a5e784abb02757003d408d3c1c836e478fe5
Vitaliy Saburdo <vsaburdo@gmail.com>
2024-07-16 12:26:34 +0300

    add result

--- main.js
+++ main.js
@@ -4,9 +4,17 @@ const inputsSymbol = document.querySelectorAll('.side_indicator');
 const mortgageAmount = document.getElementById('amount');
 const mortgageTerm = document.getElementById('term');
 const interestRate = document.getElementById('rate');
-const mortgageType = document.querySelector('input[name="mortgage-type"]');
+const mortgageTypes = document.querySelectorAll('input[name="mortgage-type"]');
 const calculateBtn = document.getElementById('btn');
 const errorMessage = document.querySelectorAll('.error_message');
+const clearAll = document.querySelector('.link');
+const answerContainer = document.querySelector('.answer_container')
+
+const fields = [
+  { element: mortgageAmount, errorId: 'amount_error', symbolId: 'amount_symbol' },
+  { element: mortgageTerm, errorId: 'term_error', symbolId: 'term_symbol' },
+  { element: interestRate, errorId: 'rate_error', symbolId: 'rate_symbol' },
+];
 
 form.addEventListener('submit', handleSubmit);
 
@@ -16,19 +24,12 @@ function handleSubmit(event) {
   const term = parseInt(form.elements.term.value);
   const rate = parseFloat(form.elements.rate.value);
 
-  let valid = true;
-  const fields = [
-    { element: mortgageAmount, errorId: 'amount_error', symbolId: 'amount_symbol' },
-    { element: mortgageTerm, errorId: 'term_error', symbolId: 'term_symbol' },
-    { element: interestRate, errorId: 'rate_error', symbolId: 'rate_symbol' },
-  ];
-
   fields.forEach(({ element, errorId, symbolId }) => {
     if (!element.value) {
       document.getElementById(errorId).style.display = 'block';
       element.classList.add('input_error');
       document.getElementById(symbolId).classList.add('side_indicator_error');
-      valid = false;
+      return;
     } else {
       document.getElementById(errorId).style.display = 'none';
       element.classList.remove('input_error');
@@ -36,15 +37,53 @@ function handleSubmit(event) {
     }
   });
 
-    if(!mortgageType.checked) {
-      document.getElementById('type_error').style.display = 'block';
-      document.querySelectorAll('.input_wrapper').forEach(item => item.style.borderColor = 'hsl(4, 69%, 50%)')
-    } else {
-      document.getElementById('type_error').style.display = 'none';
-      document.querySelectorAll('.input_wrapper').forEach(item => item.style.borderColor = 'hsl(200, 24%, 40%)');
-    }
+  const isMortgageTypeChecked = Array.from(mortgageTypes).some(radio => radio.checked);
+
+  if (!isMortgageTypeChecked) {
+    document.getElementById('type_error').style.display = 'block';
+    document
+      .querySelectorAll('.input_wrapper')
+      .forEach(item => (item.style.borderColor = 'hsl(4, 69%, 50%)'));
+    return;
+  } else {
+    document.getElementById('type_error').style.display = 'none';
+    document
+      .querySelectorAll('.input_wrapper')
+      .forEach(item => (item.style.borderColor = 'hsl(200, 24%, 40%)'));
+  }
 
   const monthlyRate = rate / 100 / 12;
   const monthlyPayment = (amount * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -term));
-  const totalPayment = monthlyPayment * term;
+  const totalPayment = (monthlyPayment * term).toFixed(2);
+  console.log(totalPayment);
+
+  answerContainer.innerHTML = `
+  <div class="valid_answer">
+  <h2>Your result</h2>
+  </div>
+  `
+}
+
+clearAll.addEventListener('click', onClearAll);
+
+function onClearAll(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+
+  mortgageTypes.forEach(radio => {
+    radio.checked = false;
+  });
+
+  fields.forEach(({ element, errorId, symbolId }) => {
+    document.getElementById(errorId).style.display = 'none';
+    element.classList.remove('input_error');
+    document.getElementById(symbolId).classList.remove('side_indicator_error');
+    document.getElementById('type_error').style.display = 'none';
+    document
+      .querySelectorAll('.input_wrapper')
+      .forEach(item => (item.style.borderColor = 'hsl(200, 24%, 40%)'));
+  });
 }
